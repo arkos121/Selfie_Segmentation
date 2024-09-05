8d37eaef854d75a66a36073d5b9cf47f98b69912
Anurag Shrestha <anuragshrestha@titan.co.in>
2024-09-05 15:43:12 +0530

    --all

--- selfiesegmentation/app/src/main/java/com/example/selfiesegmentation/MainActivity.kt
+++ selfiesegmentation/app/src/main/java/com/example/selfiesegmentation/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.example.selfiesegmentation
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Button
+import android.Manifest
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.provider.MediaStore
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -13,21 +20,51 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.selfiesegmentation.ui.theme.SelfieSegmentationTheme
+import java.io.InputStream
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+  //  override fun onCreate(savedInstanceState: Bundle?) {
+        //super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.layout)
-        val readFileButton: Button = findViewById(R.id.button1)
-        readFileButton.setOnClickListener{
-            val filecontent=
+        //setContentView(R.layout.layout)
+        lateinit var imageView: ImageView
+        lateinit var button: Button
+        private val pickImage = 100
+        private var imageUri: Uri? = null
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.layout)
+            title = "KotlinApp"
+            imageView = findViewById(R.id.button)
+            button = findViewById(R.id.button1)
+            button.setOnClickListener {
+                val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+                startActivityForResult(gallery, pickImage)
+            }
+        }
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            super.onActivityResult(requestCode, resultCode, data)
+            if (resultCode == RESULT_OK && requestCode == pickImage) {
+                imageUri = data?.data
+                imageView.setImageURI(imageUri)
+            }
         }
 
-                        }
 
-@Composable
+
+
+
+
+
+
+
+
+}
+
+    @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
