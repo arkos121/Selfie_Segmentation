ac90d98f30a8a383e9066482fe5fcf46e790fd67
Akash <akashrj1857@gmail.com>
2024-09-05 16:54:53 +0530

    -all

--- selfiesegmentation/app/src/main/java/com/example/selfiesegmentation/MainActivity.kt
+++ selfiesegmentation/app/src/main/java/com/example/selfiesegmentation/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.selfiesegmentation
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -14,18 +18,30 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.selfiesegmentation.ui.theme.SelfieSegmentationTheme
+import java.io.IOException
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.layout)
         val readFileButton: Button = findViewById(R.id.button1)
-        readFileButton.setOnClickListener{
-            val filecontent=
-        }
+        val imageView: ImageView = findViewById(R.id.imageview)
 
-                        }
+        readFileButton.setOnClickListener {
+            try {
+                val assetManager = this.assets
+                val inputStream = assetManager.open("photo1.jpg")
+                val bitmap = BitmapFactory.decodeStream(inputStream)
+
+                inputStream.close()
+                imageView.setImageBitmap(bitmap)
+            }
+            catch (e: IOException){
+                e.printStackTrace()
+            }            }
+        }
+    }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
