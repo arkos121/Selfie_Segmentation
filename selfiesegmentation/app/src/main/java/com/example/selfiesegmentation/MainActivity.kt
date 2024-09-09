1fa8cb723ac67bff5856b32f787156fc8b7463a6
bharath <kokkulabharath@titan.co.in>
2024-09-09 12:26:55 +0530

    new changes

--- selfiesegmentation/app/src/main/java/com/example/selfiesegmentation/MainActivity.kt
+++ selfiesegmentation/app/src/main/java/com/example/selfiesegmentation/MainActivity.kt
@@ -6,6 +6,7 @@ import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -25,12 +26,16 @@ import com.google.mlkit.vision.objects.defaults.ObjectDetectorOptions
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+    private var loadedBitmap: Bitmap? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Store the loaded bitmap here
 
         setContentView(R.layout.layout)
         val readFileButton: Button = findViewById(R.id.button1)
         val imageView: ImageView = findViewById(R.id.imageview)
+        val processImageButton: Button = findViewById(R.id.button2)
+        val textView : TextView = findViewById(R.id.textView)
 
         readFileButton.setOnClickListener {
             try {
@@ -40,18 +45,29 @@ class MainActivity : AppCompatActivity() {
 
                 inputStream.close()
                 imageView.setImageBitmap(bitmap)
+                loadedBitmap = bitmap
 
-                processImageObjectDetection(bitmap)
+                textView.text = ""
 
             }
+
             catch (e: IOException){
                 e.printStackTrace()
                 Toast.makeText(this,"Failed to load image0",Toast.LENGTH_LONG).show()
+                textView.text = "Failed to load the image"
             }            }
+
+        processImageButton.setOnClickListener{
+            if(loadedBitmap!=null){
+                processImageObjectDetection(loadedBitmap!!,textView)
+            }else{
+                textView.text = "First load the image"
+            }
+        }
         }
 
 
-private fun processImageObjectDetection(bitmap: Bitmap){
+private fun processImageObjectDetection(bitmap: Bitmap,textView: TextView){
     val options = ObjectDetectorOptions.Builder()
         .setDetectorMode(ObjectDetectorOptions.SINGLE_IMAGE_MODE)
         .enableMultipleObjects()
@@ -63,8 +79,9 @@ private fun processImageObjectDetection(bitmap: Bitmap){
     val inputImage = InputImage.fromBitmap(bitmap,0)
 
     objectdetector.process(inputImage).addOnSuccessListener { detectedObjects->
-        if(detectedObjects.isEmpty())
+        if(detectedObjects.isEmpty()){
             Toast.makeText(this,"No object detected",Toast.LENGTH_SHORT).show()
+              textView.text ="No object detected"}
     else{
         for(obj in detectedObjects){
             val box = obj.boundingBox
@@ -72,10 +89,11 @@ private fun processImageObjectDetection(bitmap: Bitmap){
             val labels = obj.labels
 
             Toast.makeText(this,"Object detected at : $box",Toast.LENGTH_SHORT).show()
-
+            textView.append("Object at ${obj.boundingBox}\n")
             if(labels.isNotEmpty()){
                 val label = labels[0].text
-                Toast.makeText(this,"detected label: $labels",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this,"detected label: $label",Toast.LENGTH_SHORT).show()
+                textView.append("Label : label\n")
             }
         }
     }
