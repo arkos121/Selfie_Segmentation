a507c5f6b6591b84777f1018853368eff59774cb
bharath <kokkulabharath@titan.co.in>
2024-09-09 11:31:56 +0530

    -all

--- selfiesegmentation/app/src/main/java/com/example/selfiesegmentation/MainActivity.kt
+++ selfiesegmentation/app/src/main/java/com/example/selfiesegmentation/MainActivity.kt
@@ -6,6 +6,7 @@ import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -18,6 +19,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.selfiesegmentation.ui.theme.SelfieSegmentationTheme
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.objects.ObjectDetection
+import com.google.mlkit.vision.objects.defaults.ObjectDetectorOptions
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
@@ -36,25 +40,50 @@ class MainActivity : AppCompatActivity() {
 
                 inputStream.close()
                 imageView.setImageBitmap(bitmap)
+
+                processImageObjectDetection(bitmap)
+
             }
             catch (e: IOException){
                 e.printStackTrace()
+                Toast.makeText(this,"Failed to load image0",Toast.LENGTH_LONG).show()
             }            }
+        }
+
+
+private fun processImageObjectDetection(bitmap: Bitmap){
+    val options = ObjectDetectorOptions.Builder()
+        .setDetectorMode(ObjectDetectorOptions.SINGLE_IMAGE_MODE)
+        .enableMultipleObjects()
+        .enableClassification()
+        .build()
+
+    val objectdetector = ObjectDetection.getClient(options)
+
+    val inputImage = InputImage.fromBitmap(bitmap,0)
+
+    objectdetector.process(inputImage).addOnSuccessListener { detectedObjects->
+        if(detectedObjects.isEmpty())
+            Toast.makeText(this,"No object detected",Toast.LENGTH_SHORT).show()
+    else{
+        for(obj in detectedObjects){
+            val box = obj.boundingBox
+            val trackid = obj.trackingId
+            val labels = obj.labels
+
+            Toast.makeText(this,"Object detected at : $box",Toast.LENGTH_SHORT).show()
+
+            if(labels.isNotEmpty()){
+                val label = labels[0].text
+                Toast.makeText(this,"detected label: $labels",Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SelfieSegmentationTheme {
-        Greeting("Android")
+        .addOnFailureListener{ e->
+            e.printStackTrace()
+            Toast.makeText(this,"ObjectDetectionFailed", Toast.LENGTH_SHORT).show()
+        }
+}
     }
-}
