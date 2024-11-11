544894fb78bdb103dd48bb6be316d6188c58c4be
Akash <akashrj1857@gmail.com>
2024-11-12 01:18:45 +0530

    did changes

--- selfiesegmentation/app/src/main/java/com/example/selfiesegmentation/MyAdapter.kt
+++ selfiesegmentation/app/src/main/java/com/example/selfiesegmentation/MyAdapter.kt
@@ -10,9 +10,9 @@ import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import java.io.IOException
 
-class MyAdapter(private var dataListitem : List<String>,private var context : Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class MyAdapter(private var dataListitem: List<String>, private var context: Context,private var copyImageLocation : (String) -> Unit) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+     class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val imageView: ImageView = itemView.findViewById(R.id.imageView)
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
@@ -26,21 +26,26 @@ class MyAdapter(private var dataListitem : List<String>,private var context : Co
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
        val bitmap = getBitmapFromUri(dataListitem[position])
+        val imageloc = dataListitem[position]
         (holder as MyViewHolder).imageView.setImageBitmap(bitmap)
 
+        holder.imageView.setOnClickListener {
+            copyImageLocation(imageloc)
+        }
     }
 
-    private fun getBitmapFromUri(s: String): Bitmap?{
-       return try{
-           val assetManager = context.assets
-           val inputStream = assetManager.open(s)
-           val bitmap = BitmapFactory.decodeStream(inputStream)
-           inputStream.close()
-           bitmap
-       }catch (e : IOException){
-           e.printStackTrace()
-           return null
-       }
+    private fun getBitmapFromUri(s: String): Bitmap?
+    {
+        return try {
+            val assetManager = context.assets
+            val inputStream = assetManager.open(s)
+            val bitmap = BitmapFactory.decodeStream(inputStream)
+            inputStream.close()
+            bitmap
+        } catch (e: IOException) {
+            e.printStackTrace()
+            return null
+        }
     }
 
 }
